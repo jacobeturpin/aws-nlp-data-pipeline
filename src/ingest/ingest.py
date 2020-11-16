70e1da3055a8c8313b84fa5bfda8ea50e329a938
jacobeturpin <jacobeturpin@gmail.com>
2020-11-15 20:08:30 -0500

    add more debug

--- src/ingest/ingest.py
+++ src/ingest/ingest.py
@@ -10,6 +10,13 @@ DELIVERY_STREAM_NAME = os.environ['STREAM_NAME']
 def lambda_handler(event, context):
 
     print(event['data'])
+
+    print([
+        {
+            'Data': json.dumps(record)
+        }
+        for record in event['data']
+    ])
     
     client = boto3.client('firehose')
 
