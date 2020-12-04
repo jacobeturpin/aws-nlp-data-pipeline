19fa65d82c538e56d6a865ec3fba5c85b3dec830
jacobeturpin <jacobeturpin@gmail.com>
2020-12-03 21:27:51 -0500

    remove unused shell script

--- create-stack.sh
+++
@@ -1,7 +0,0 @@
-aws cloudformation create-stack --stack-name test \
-    --template-body file://storage-stack.yml \
-    --parameters ParameterKey=LogBucketName,ParameterValue=mybucketname \
-                 ParameterKey=ElasticsearchDomainName,ParameterValue=esdomainname \
-                 ParameterKey=ElasticsearchIndexName,ParameterValue=test \
-                 ParameterKey=FirehoseName,ParameterValue=myirehosename \
-    --capabilities CAPABILITY_NAMED_IAM
